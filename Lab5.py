1a0671479db95fd9a81ecd1f389e5d02ce625d87
NathanPatel <2492881@edu.vaniercollege.qc.ca>
2026-01-10 22:24:33 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -30,9 +30,11 @@ def number_pattern(n):
     for i in range(1, n + 1):
         for j in range(1, i + 1):
             result += str(j)
-        result += "\n"
+        if i != n:            
+            result += "\n"
 
     return result
+
 
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
