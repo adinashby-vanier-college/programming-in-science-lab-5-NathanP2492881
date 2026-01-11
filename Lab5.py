4ca755495cad621d550fec8e7685af14d547c2e7
NathanPatel <2492881@edu.vaniercollege.qc.ca>
2026-01-10 22:12:38 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,43 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    result = ""
+
+    for i in range(n):          # rows
+        for j in range(n):      # columns
+            if i == 0 or i == n-1 or j == 0 or j == n-1:
+                result += "*"
+            else:
+                result += " "
+        result += "\n"
+
+    return result
+
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ""
+
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            result += str(j)
+        result += "\n"
+
+    return result
+
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    total = 0
+
+    for i in range(1, n + 1):
+        total += i
+
+    return total
+
 
 # Example for n = 4:
 #    *
